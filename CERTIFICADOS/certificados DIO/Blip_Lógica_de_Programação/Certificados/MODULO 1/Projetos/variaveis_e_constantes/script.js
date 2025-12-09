dc4e29beeccfe5c89dfa144a5872e66346ac6118
Pedro Martins <seu-email@gmail.com>
2025-12-09 17:25:43 -0300

    subindo Blip_Lógica_de_Programação

---
+++ CERTIFICADOS/certificados DIO/Blip_Lógica_de_Programação/Certificados/MODULO 1/Projetos/variaveis_e_constantes/script.js
@@ -0,0 +1,9 @@
+//exibr uma mensagem no console
+console.log("Digite o nome do seu jogador")
+
+// declara a variavel
+let nickname ="Pedro mxr"
+
+//concatena a variavel com o texto
+console.log("Bem vindo ao jogo" + nickname)
+console.log(nickname + "entrou no jogo")
---
+++ CERTIFICADOS/certificados DIO/Blip_Lógica_de_Programação/Certificados/MODULO 1/Projetos/variaveis_e_constantes/script5.js
@@ -0,0 +1,11 @@
+// nome, idade, numero de telefone, cpf, endereço, recebePensao
+//string = armazena texto
+// number = armazena numeros
+// boolean = armazena valores logicos
+
+let nomePessoa = "Iolanda amaral"
+let idade = 75
+let numeroTelefone = "(17) 98471-3388"
+let cpf = "321.321.321-32"
+let endereco = "Rua das flores, numero 321, bairro jardim das rosas"
+let recebePensao = true
