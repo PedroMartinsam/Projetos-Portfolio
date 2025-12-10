4e38d8a48da338bc95606a301bd156b00777cc28
Pedro Martins <seu-email@gmail.com>
2025-12-09 23:24:42 -0300

    Subindo modulo 2

---
+++ CERTIFICADOS/certificados DIO/Blip_Lógica_de_Programação/Certificados/MODULO 2/Projetos/Operadores_de_Comparação/script2.js
@@ -0,0 +1,5 @@
+//guardar o valor em uma variável de resultado TRUE?FALSE
+let marca = "Apple"
+let resultado = marca === "Samsung"
+
+console.log(resultado)
---
+++ CERTIFICADOS/certificados DIO/Blip_Lógica_de_Programação/Certificados/MODULO 2/Projetos/Operadores_de_Comparação/script4.js
@@ -0,0 +1,6 @@
+let CPFPermitido = "222.555.333-02"
+let CPFDoUsuario = "222.555.333-02"
+
+let ehBloqueado = CPFDoUsuario !== CPFPermitido
+
+console.log("é um usuario invalido ? " + ehBloqueado)
