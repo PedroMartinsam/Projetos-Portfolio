4cc3de865cc376ea7a87844155a4413fe9b16d68
Pedro Martins <seu-email@gmail.com>
2025-12-10 12:11:32 -0300

    subindo modulo 4

---
+++ CERTIFICADOS/certificados DIO/Blip_Lógica_de_Programação/Certificados/MODULO 4/Projetos/funcao/script2.js
@@ -0,0 +1,12 @@
+torrar()
+
+
+function torrar(){
+ console.log("torrando pão")
+ injetarPao()
+}
+
+function injetarPao(){
+	console.log("preparando para injetar o pão")
+    console.log("finalizado")
+}
---
+++ CERTIFICADOS/certificados DIO/Blip_Lógica_de_Programação/Certificados/MODULO 4/Projetos/funcao_com_parametros/script.js
@@ -0,0 +1,7 @@
+function calcularIdade(nome, anoNascimento) {
+  let idade = 2023 - anoNascimento;
+  console.log(nome + " tem " + idade + " anos!");
+}
+
+calcularIdade("Pedrinho", 2010); // Vai mostrar Pedrinho tem 13 anos!
+calcularIdade("Mariana", 2008); // Vai mostrar Mariana tem 15 anos!
